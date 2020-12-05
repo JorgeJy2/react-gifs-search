5434c0f17e060c232bd5bdc17e9ef162fe223f3c
jorge jacobo <jorge.jacobo.francisco.306@gmail.com>
2020-12-04 18:28:44 -0600

    fix: set default params

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import GifExpertApp from './GifExpertApp';
+import {GifExpertApp} from './GifExpertApp';
 import './index.css';
 
  ReactDOM.render(
-  <GifExpertApp/>,
+  <GifExpertApp defaultCategories= { ['cats' , 'programing']}/>,
    document.getElementById('root')
  );
